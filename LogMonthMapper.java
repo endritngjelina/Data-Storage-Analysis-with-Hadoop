9a3733e840c46d40ef42822680773233be7c26b8
Endrit Ngjelina <121914667+endritngjelina@users.noreply.github.com>
2025-07-05 05:26:46 -0400

    Add files via upload

---
+++ LogMonthMapper.java
@@ -0,0 +1,34 @@
+package stubs;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class LogMonthMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+  /**
+   * Example input line:
+   * 96.7.4.14 - - [24/Apr/2011:04:20:11 -0400] "GET /cat.jpg HTTP/1.1" 200 12433
+   *
+   */
+  @Override
+  
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+    
+	  String line = value.toString();
+	  Pattern ipPattern = Pattern.compile("(\\A|\\s)(?:(?:25[0-5]|2[0-4]\\d|[01]?\\d{1,2})\\.){3}(?:25[0-5]|2[0-4]\\d|[01]?\\d{1,2})(\\s|\\z)");
+      Pattern monthPattern = Pattern.compile("(?<=\\[(([0-2]?\\d)|([3][0-2]))/)([A-Z][a-z]{2})(?=/(([1][9]{2}\\d)|([2][0][01]\\d)).*\\])");
+      Matcher ip = ipPattern.matcher(line);
+	  Matcher month = monthPattern.matcher(line);
+	  if (ip.find() && month.find()){
+		  context.write(new Text(ip.group().trim()), new Text(month.group().toLowerCase().trim()));
+	  } 
+  }
+}
