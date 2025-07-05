06ad7e4ab3fcf60049842dbd15c0f86848282608
Endrit Ngjelina <121914667+endritngjelina@users.noreply.github.com>
2025-07-05 05:26:32 -0400

    Add files via upload

---
+++ CountReducer.java
@@ -0,0 +1,20 @@
+package stubs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import com.google.common.collect.Iterables;
+
+public class CountReducer extends Reducer<Text, Text, Text, IntWritable> {
+
+  @Override
+  public void reduce(Text key, Iterable<Text> values, Context context)
+      throws IOException, InterruptedException {
+	  int counter= Iterables.size(values);
+	  context.write(key, new IntWritable(counter));
+  }
+}
